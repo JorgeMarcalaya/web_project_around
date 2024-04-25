7195c98a0e3c9d2e16ceecead37e4cebece97366
Jorge <jorgemarcalaya@gmail.com>
2024-04-24 19:22:20 -0500

    Primer commit Sprint5

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Tripleten web_project_around
+
+El proyecto esta desarrollado en html 5 y css, diseñado en 3 dimensiones estandar 1280px,880px y 320px totalmente responsiva. La pagina consta de un blog de fotos de lugares muy conocidos de un usuario. En funcionalidad el usuario puedo editar campos como nombre e descripcion mediante popus que cuentan con un formulario totalmente centrado y fijado. Tambien puedo cerrar los popus.  
+
+Tecnicas y Tecnologias utilizadas:
+Figma una herramienta donde nos enseñan como se debe distribuir la pagina web y sus elementos. Cuenta con version online y en escritorio.
+Git hub es una plataforma de desarrollo colaborativo basada en Git, un sistema de control de versiones. GitHub proporciona herramientas y servicios que facilitan la colaboración en proyectos de desarrollo de software.
+Vscode es un editor de código fuente gratuito y de código abierto desarrollado por Microsoft.
+Metodologia BEM divide la interfaz de usuario en bloques independientes para crear componentes escalables y reutilizables.
+Tinypng es un servicio en línea que se utiliza para comprimir imágenes, reduciendo su tamaño de archivo sin comprometer significativamente la calidad visual.
+Javascript es un lenguaje de programacion que te permite implementar funciones mas complejas en paginas web.
+
+https://github.com/JorgeMarcalaya/web_project_around.git
---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+//Variables
+const button1 = document.querySelector("#btnedit");
+const button2 = document.querySelector("#btnadd");
+const button3 = document.querySelector("#profile");
+const button4 = document.querySelector("#add");
+const profileName = document.querySelector("#name");
+const profileAbout = document.querySelector("#about");
+const popupProfile = document.querySelector(".popup__content_profile");
+const nameProfile = document.querySelector(".nav__profile-nombre");
+const aboutProfie = document.querySelector(".nav__profile-about");
+const popupAddCard = document.querySelector(".popup__content_add-card");
+let formElement = document.querySelector("#form__edit");
+
+// Eventos
+button1.addEventListener("click", popup__open_edit);
+button2.addEventListener("click", popup__open_add);
+button3.addEventListener("click", popup__close_profile);
+button4.addEventListener("click", popup__close_add);
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+//Funciones
+function popup__open_edit() {
+  profileName.placeholder = nameProfile.textContent;
+  profileAbout.placeholder = aboutProfie.textContent;
+  popupProfile.classList.add("popup__opened");
+}
+function popup__open_add() {
+  popupAddCard.classList.add("popup__opened");
+}
+function popup__close_profile() {
+  popupProfile.classList.remove("popup__opened");
+}
+function popup__close_add() {
+  popupAddCard.classList.remove("popup__opened");
+}
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  let namevalue = document.querySelector("#name");
+  let aboutvalue = document.querySelector("#about");
+  nameProfile.textContent = namevalue.value;
+  aboutProfie.textContent = aboutvalue.value;
+  popupProfile.classList.remove("popup__opened");
+}
